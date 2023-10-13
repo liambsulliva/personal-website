d1b9230f0ec3f781288ea473328be435052513ca
Liam Sullivan <liambsulliva@Liams-MacBook-Air-3.local>
2023-10-13 13:10:39 -0400

    fixed socials in darkmode

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,6 +1,8 @@
 let darkMode = localStorage.getItem('darkMode'); //mutable
-const darkModeToggle = document.querySelector('#dark-mode-toggle');
-const darkModeImage = document.querySelector('#dark-mode-image');
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const darkModeImage = document.getElementById('dark-mode-image');
+const darkModeIcon1 = document.getElementById('dark-mode-icon1');
+const darkModeIcon2 = document.getElementById('dark-mode-icon2');
 
 // check if darkmode is enabled
 // flip states
@@ -18,6 +20,8 @@ const disableDarkMode = () => {
 if(darkMode === 'enabled'){
     enableDarkMode();
     darkModeImage.src = 'Images/sun.svg';
+    darkModeIcon1.src = 'Images/icon1-dark.svg';
+    darkModeIcon2.src = 'Images/icon2-dark.svg';
 }
 
 darkModeToggle.addEventListener('click', () => {
@@ -26,9 +30,13 @@ darkModeToggle.addEventListener('click', () => {
         enableDarkMode();
         darkMode = 'enabled';
         darkModeImage.src = 'Images/sun.svg';
+        darkModeIcon1.src = 'Images/icon1-dark.svg';
+        darkModeIcon2.src = 'Images/icon2-dark.svg';
     } else {
         disableDarkMode();
         darkMode = 'disabled';
         darkModeImage.src = 'Images/moon.svg';
+        darkModeIcon1.src = 'Images/icon1.svg';
+        darkModeIcon2.src = 'Images/icon2.svg';
     }
 })
