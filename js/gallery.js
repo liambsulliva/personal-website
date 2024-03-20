3e0b1699e2cb26aa4a0d97a628aa0a06126da316
Liam <{ID}+{username}@users.noreply.github.com>
2024-03-19 23:20:01 -0400

    hardcoded key

--- js/gallery.js
+++ js/gallery.js
@@ -1,8 +1,8 @@
+const myKey = 'a52e2fe91b943c26733c493a6c3c3090';
 jQuery("#photo-gallery").nanogallery2({
     kind:             'flickr',
     userID:           '148052898@N03',
-    flickrAPIKey:     MY_KEY,
-    secretkey:        SECRET_KEY,
+    flickrAPIKey:     myKey,
     photoset: "none",
     thumbnailL1Height:  300,
     thumbnailL1Width:   'auto',
